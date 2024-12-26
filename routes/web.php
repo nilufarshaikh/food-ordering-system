b90a5a0621bf070b130ec16af2e8cbe006df7fd0
Nilufar Shaikh <nilufarrehanshaikh@gmail.com>
2024-12-26 13:20:16 -0500

    Separated admin routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +18,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-Route::get('admin/dashboard', [AdminDashboardController::class, 'index'])->middleware("auth", "role:admin")->name('admin.dashboard');
